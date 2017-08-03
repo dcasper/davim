22d748bb79711c6ce451f20e292c5d015e3b35d0
David Casper <davidcasper00@gmail.com>
2017-08-03 00:20:56 -0700

    Add support for rbenv in vagrantfile

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,14 +1,20 @@
 #! /usr/bin/env bash
 
-# Aptitude Installs
+# Add postgres repo for upgrading to pg 9.6
+echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main" >> /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
+    apt-key add -
 apt-get update
-apt-get install -y git screen zsh npm
 
-# Get RVM hooked up and on its feet
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-source /home/vagrant/.rvm/scripts/rvm
-sudo chmod a+rw -R /usr/local/rvm/
+# Aptitude Installs
+apt-get install -y git screen zsh jq awscli \
+ postgresql-9.6 libpq-dev \
+ libssl-dev libreadline-dev zlib1g-dev \
+ npm
+
+# Postgres Setup
+su postgres -c "createuser vagrant"
+su postgres -c "createdb vagrant"
 
 # Oh My Zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git
@@ -22,6 +28,13 @@ ln -s davim/.vimrc .vimrc
 ln -s davim/.zshrc .zshrc
 ln -s davim/.zshenv .zshenv
 ln -s davim/.screenrc .screenrc
+
+# Install rbenv and plugins/current ruby
+su vagrant -c 'git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv'
+su vagrant -c 'git clone https://github.com/rbenv/ruby-build.git $HOME/.rbenv/plugins/ruby-build'
+sudo -H -u vagrant zsh -i -c 'rbenv install 2.3.4'
+sudo -H -u vagrant zsh -i -c 'rbenv global 2.3.4'
+sudo -H -u vagrant zsh -i -c 'gem install rails -v 4.2.6'
 
 # Heroku Toolbelt
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
