dfcea485eeccd851f19df22f3fa3bfed4c1f8754
David Casper <dcasper@twitter.com>
2019-05-02 11:27:53 -0700

    Add .proprietary as an empty dir

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
+# Must be run from the config source dir
 davim=$(pwd)
 cd ~
 ln -s $davim/.vim .vim
@@ -7,8 +8,3 @@ ln -s $davim/.vimrc .vimrc
 ln -s $davim/.screenrc .screenrc
 ln -s $davim/.zshenv .zshenv
 ln -s $davim/.zshrc .zshrc
-
-ln -s $davim/.portal-screenrc .portal-screenrc
-ln -s $davim/.connector-screenrc .connector-screenrc
-ln -s $davim/screen/screen_setup.sh .screen_setup.sh
-ln -s $davim/screen/screen_setup.rb .screen_setup.rb
